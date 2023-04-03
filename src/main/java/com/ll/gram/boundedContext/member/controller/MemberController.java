0d08bb2e9168fc086d881c488eaf7e3846f134fb
parkchaewon18 <0101angela@naver.com>
2023-04-04 03:24:33 +0900

    회원가입 폼 구현

--- src/main/java/com/ll/gram/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/gram/boundedContext/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.ll.gram.boundedContext.member.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +13,10 @@ public class MemberController {
     @GetMapping("/join")
     public String showJoin() {
         return "usr/member/join";
+    }
+    @PostMapping("/join")
+    public String join() {
+        return "redirect:/";
     }
 }
 
--- src/test/java/com/ll/gram/com/ll/gram/boundedContext/member/controller/MemberControllerTests.java
+++ src/test/java/com/ll/gram/com/ll/gram/boundedContext/member/controller/MemberControllerTests.java
@@ -1,6 +1,6 @@
-package com.ll.gram.com.ll.gram.boundedContext.member.controller;
+package com.ll.gram.boundedContext.member.controller;
 
-import com.ll.gram.boundedContext.member.controller.MemberController;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +11,19 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@Transactional
-@ActiveProfiles("test")
-
+@SpringBootTest // 스프링부트 관련 컴포넌트 테스트할 때 붙여야 함, Ioc 컨테이너 작동시킴
+@AutoConfigureMockMvc // http 요청, 응답 테스트
+@Transactional // 실제로 테스트에서 발생한 DB 작업이 영구적으로 적용되지 않도록, test + 트랜잭션 => 자동롤백
+@ActiveProfiles("test") // application-test.yml 을 활성화 시킨다.
 public class MemberControllerTests {
     @Autowired
     private MockMvc mvc;
@@ -31,16 +34,43 @@ public class MemberControllerTests {
         // WHEN
         ResultActions resultActions = mvc
                 .perform(get("/member/join"))
-                .andDo(print());
+                .andDo(print()); // 크게 의미 없고, 그냥 확인용
 
         // THEN
         resultActions
                 .andExpect(handler().handlerType(MemberController.class))
                 .andExpect(handler().methodName("showJoin"))
-                .andExpect(status().is2xxSuccessful());
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().string(containsString("""
+                        <input type="text" name="username"
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        <input type="password" name="password"
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        <input type="submit" value="회원가입"
+                        """.stripIndent().trim())));
+
     }
 
+    @Test
+    @DisplayName("회원가입")
+    void t002() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(post("/member/join")
+                        .with(csrf()) // CSRF 키 생성
+                        .param("username", "user10")
+                        .param("password", "1234")
+                )
+                .andDo(print());
 
+        // THEN
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("join"))
+                .andExpect(status().is3xxRedirection());
+    }
 }
 
 
