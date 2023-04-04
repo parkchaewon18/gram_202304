ef6bf1d184d3f98e0e87779bfcfee206e060faae
parkchaewon18 <0101angela@naver.com>
2023-04-04 15:22:50 +0900

    로그인 폼 처리 테스트케이스에서 세션까지 체크하기

--- src/test/java/com/ll/gram/com/ll/gram/boundedContext/member/controller/MemberControllerTests.java
+++ src/test/java/com/ll/gram/com/ll/gram/boundedContext/member/controller/MemberControllerTests.java
@@ -1,14 +1,21 @@
 package com.ll.gram.boundedContext.member.controller;
 
+
+import com.ll.gram.boundedContext.member.controller.MemberController;
 import com.ll.gram.boundedContext.member.entity.Member;
 import com.ll.gram.boundedContext.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -146,7 +153,7 @@ public class MemberControllerTests {
         // WHEN
         ResultActions resultActions = mvc
                 .perform(get("/member/login"))
-                .andDo(print()); // 크게 의미 없고, 그냥 확인용
+                .andDo(print());
 
         // THEN
         resultActions
@@ -176,6 +183,14 @@ public class MemberControllerTests {
                         .param("password", "1234")
                 )
                 .andDo(print());
+
+        // 세션에 접근해서 user 객체를 가져온다.
+        MvcResult mvcResult = resultActions.andReturn();
+        HttpSession session = mvcResult.getRequest().getSession(false);// 원래 getSession 을 하면, 만약에 없을 경우에 만들어서라도 준다., false 는 없으면 만들지 말라는 뜻
+        SecurityContext securityContext = (SecurityContext)session.getAttribute("SPRING_SECURITY_CONTEXT");
+        User user = (User)securityContext.getAuthentication().getPrincipal();
+
+        assertThat(user.getUsername()).isEqualTo("user1");
 
         // THEN
         resultActions
