5c4d171fb197db2df608bbf858753a1488ffc083
parkchaewon18 <0101angela@naver.com>
2023-04-04 15:36:01 +0900

    관리자 페이지

--- src/main/java/com/ll/gram/base/security/SecurityConfig.java
+++ src/main/java/com/ll/gram/base/security/SecurityConfig.java
@@ -1,13 +1,16 @@
 package com.ll.gram.base.security;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
